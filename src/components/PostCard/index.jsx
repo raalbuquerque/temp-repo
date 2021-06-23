c7b995b942d7c18878c43c9d55928da860ac9d6f
= <=>
2021-06-23 11:31:35 -0300

    Initial

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const PostCard = ({ post }) => (
+    // const { post } = props;
+    <div className="post">
+        <img src={post.cover} alt={post.title}></img>
+        <div className="post-content">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+        </div>
+    </div>
+)
